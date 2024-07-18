e3e99ea73c62c3b5f08dd9c502f0510679b279de
AmrMossad93 <amr.mossad@altheqa.com>
2024-07-18 12:14:39 +0300

    Auto-generated commit [ci skip]

--- assets/js/main.js
+++ assets/js/main.js
@@ -79,30 +79,30 @@
   /**
    * Scroll top button
    */
-  document.addEventListener('DOMContentLoaded', function() {
-    let scrollTop = document.querySelector('.scroll-top');
-
-    function toggleScrollTop() {
-      if (scrollTop) {
-        window.scrollY > 100 ? scrollTop.classList.add('active') : scrollTop.classList.remove('active');
-      }
-    }
-
-    if (scrollTop) {
-      scrollTop.addEventListener('click', (e) => {
-        e.preventDefault();
-        window.scrollTo({
-          top: 0,
-          behavior: 'smooth'
-        });
-      });
-
-      window.addEventListener('load', toggleScrollTop);
-      document.addEventListener('scroll', toggleScrollTop);
-    } else {
-      console.error('scrollTop element is null');
-    }
-  });
+  // document.addEventListener('DOMContentLoaded', function() {
+  //   let scrollTop = document.querySelector('.scroll-top');
+  //
+  //   function toggleScrollTop() {
+  //     if (scrollTop) {
+  //       window.scrollY > 100 ? scrollTop.classList.add('active') : scrollTop.classList.remove('active');
+  //     }
+  //   }
+  //
+  //   if (scrollTop) {
+  //     scrollTop.addEventListener('click', (e) => {
+  //       e.preventDefault();
+  //       window.scrollTo({
+  //         top: 0,
+  //         behavior: 'smooth'
+  //       });
+  //     });
+  //
+  //     window.addEventListener('load', toggleScrollTop);
+  //     document.addEventListener('scroll', toggleScrollTop);
+  //   } else {
+  //     console.error('scrollTop element is null');
+  //   }
+  // });
 
   /**
    * Animation on scroll function and init
