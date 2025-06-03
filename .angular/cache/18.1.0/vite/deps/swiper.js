09fb78c299b6c63a2f4eb1c924a84eaa7fe78124
AmrMossad93 <ahussein.c@tahakom.com>
2025-06-04 01:11:24 +0300

    NF: Ananas

---
+++ .angular/cache/18.1.0/vite/deps/swiper.js
@@ -0,0 +1,10 @@
+import {
+  Swiper
+} from "./chunk-4TAGQ2BO.js";
+import "./chunk-ZCJ6KRUD.js";
+import "./chunk-3OV72XIM.js";
+export {
+  Swiper,
+  Swiper as default
+};
+//# sourceMappingURL=swiper.js.map
