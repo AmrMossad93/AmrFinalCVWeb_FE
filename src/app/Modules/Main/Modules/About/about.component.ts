4a725687250867ed23bcd91774d47772eedfd64d
Amr Mossad <ahussein.c@tahakom.com>
2026-02-01 23:33:43 +0300

    Add certificates section to About page with support for listing professional achievements and displaying certificate details

--- src/app/Modules/Main/Modules/About/about.component.ts
+++ src/app/Modules/Main/Modules/About/about.component.ts
@@ -8,4 +8,47 @@ import { Component } from '@angular/core';
 })
 export class AboutComponent {
   yearsOfExperience: number = new Date().getFullYear() - 2017;
+
+  certificates = [
+    {
+      name: 'AI Workshop',
+      path: 'Images/Cir/69a65449-c2d5-412f-a666-b21cf9b9a701.jpg'
+    },
+    {
+      name: 'Developing ASP.NET MVC 4 Web Applications',
+      path: 'Images/Cir/20200131230628761_Page_01.jpg'
+    },
+    {
+      name: 'Programming in HTML5 with JavaScript and CSS3',
+      path: 'Images/Cir/20200131230628761_Page_02.jpg'
+    },
+    {
+      name: 'Developing Windows Azure and Web Services',
+      path: 'Images/Cir/20200131230628761_Page_03.jpg'
+    },
+    {
+      name: 'Microsoft Web Development Diploma',
+      path: 'Images/Cir/20200131230628761_Page_04.jpg'
+    },
+    {
+      name: 'MCSD: App Builder Solutions Developer',
+      path: 'Images/Cir/20200131230628761_Page_05.jpg'
+    },
+    {
+      name: 'Mean Stack & IONIC',
+      path: 'Images/Cir/20200131230628761_Page_06.jpg'
+    },
+    {
+      name: 'Certificate of Congratulations',
+      path: 'Images/Cir/20200131230628761_Page_07.jpg'
+    },
+    {
+      name: 'Training College Britain',
+      path: 'Images/Cir/20200131230628761_Page_09.jpg'
+    },
+    {
+      name: 'Cyber Security',
+      path: 'Images/Cir/Cyper.png'
+    }
+  ];
 }
