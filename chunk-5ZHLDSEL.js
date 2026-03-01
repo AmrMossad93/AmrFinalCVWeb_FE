318bcd4d92f070a39358487fb3051f65c17b7153
Amr Mossad <50120275+AmrMossad93@users.noreply.github.com>
2026-03-01 23:56:28 +0300

    Auto-generated commit

---
+++ chunk-5ZHLDSEL.js
@@ -0,0 +1,1 @@
+import{Ma as i,m as e,p as o}from"./chunk-CI7QKB4X.js";var a=class t{constructor(r){this.http=r}getJson(r){return this.http.get("Data/"+r)}static \u0275fac=function(n){return new(n||t)(o(i))};static \u0275prov=e({token:t,factory:t.\u0275fac,providedIn:"root"})};export{a};
---
+++ chunk-RHJAFIEZ.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-5ZHLDSEL.js";import{m as t,q as e}from"./chunk-CI7QKB4X.js";var i={HEADER:"header.json"};var r=class o{dataService=e(a);getHeader(){return this.dataService.getJson(i.HEADER)}static \u0275fac=function(m){return new(m||o)};static \u0275prov=t({token:o,factory:o.\u0275fac,providedIn:"root"})};var E=()=>e(r).getHeader();export{E as a};
