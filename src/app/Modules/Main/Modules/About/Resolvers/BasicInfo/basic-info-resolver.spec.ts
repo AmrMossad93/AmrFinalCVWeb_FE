0f2b40927742fd86a0e1010b52d121d4d320b434
Amr Mossad <50120275+AmrMossad93@users.noreply.github.com>
2026-03-01 23:25:19 +0300

    Update basic-info-resolver spec to use strong typing with IBase and IBasicInfo

--- src/app/Modules/Main/Modules/About/Resolvers/BasicInfo/basic-info-resolver.spec.ts
+++ src/app/Modules/Main/Modules/About/Resolvers/BasicInfo/basic-info-resolver.spec.ts
@@ -2,9 +2,11 @@ import { TestBed } from '@angular/core/testing';
 import { ResolveFn } from '@angular/router';
 
 import { basicInfoResolver } from './basic-info-resolver';
+import {IBase} from '../../../../../../Core/models/Interface/Base/base';
+import {IBasicInfo} from '../../DTO/Interface/BasicInfo/basic-info';
 
 describe('basicInfoResolver', () => {
-  const executeResolver: ResolveFn<boolean> = (...resolverParameters) => 
+  const executeResolver: ResolveFn<IBase<IBasicInfo>> = (...resolverParameters) =>
       TestBed.runInInjectionContext(() => basicInfoResolver(...resolverParameters));
 
   beforeEach(() => {
